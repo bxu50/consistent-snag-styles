62fe0ab5b461d452dd34e7def23087d42af10b3a
Jeffery Qi <jqi@bunnings.com.au>
2022-07-14 16:44:15 +1000

    add custom class for label

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,4 +1,5 @@
 import React, { InputHTMLAttributes } from 'react';
+import '../../lib/tailwind.scss';
 
 export interface RadioButtonProps
   extends InputHTMLAttributes<HTMLInputElement> {
@@ -6,6 +7,7 @@ export interface RadioButtonProps
   label: string;
   name: string;
   value: string;
+  className?: string;
 }
 
 const RadioButton: React.FC<RadioButtonProps> = ({
@@ -13,6 +15,7 @@ const RadioButton: React.FC<RadioButtonProps> = ({
   label,
   name,
   value,
+  className,
   ...otherProps
 }) => (
   <>
@@ -26,7 +29,7 @@ const RadioButton: React.FC<RadioButtonProps> = ({
     />
     <label
       htmlFor={id}
-      className="py-4 pl-4 pr-14 bg-white border border-grey_3 border-solid text-primary-main text-sm font-bold relative before:absolute before:w-5 before:h-5 before:rounded-full before:border before:border-common-black before:border-solid before:right-4 before:top-3.5 hover:border-black"
+      className={`py-4 pl-4 pr-14 bg-white border border-grey_3 border-solid text-primary-main text-sm font-bold relative before:absolute before:w-5 before:h-5 before:rounded-full before:border before:border-common-black before:border-solid before:right-4 before:top-3.5 hover:border-black ${className}`}
     >
       {label}
     </label>
