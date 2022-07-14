c23d6edde2149889ad49b3e05de8eb9e05ee942f
Josh Yu <jyu1@bunnings.com.au>
2022-07-14 13:18:56 +1000

    chore: Fixes package.json; removes unused files

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,7 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({ tsconfig: './tsconfig.json', exclude: ['**/stories/*', '**/*.test.ts'] }),
       postcss(),
     ],
   },
